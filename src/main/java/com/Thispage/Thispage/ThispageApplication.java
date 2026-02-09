5c6075bb371a8a33ca097d675133fd0656ae2dd7
Matheus Schingiry <matheussschingiry@gmail.com>
2026-02-09 20:36:14 -0300

    First commit

---
+++ src/main/java/com/Thispage/Thispage/ThispageApplication.java
@@ -0,0 +1,13 @@
+package com.Thispage.Thispage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThispageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThispageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Thispage/Thispage/ThispageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Thispage.Thispage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThispageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
