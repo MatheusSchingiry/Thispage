014408c1129a908f488bf6802dbcbc4419686362
Matheus Schingiry <matheussschingiry@gmail.com>
2026-02-09 22:28:34 -0300

    feat: implement service layer and web controllers

---
+++ src/main/java/com/Thispage/Thispage/Controller/PostController.java
@@ -0,0 +1,36 @@
+package com.Thispage.Thispage.Controller;
+
+import com.Thispage.Thispage.Domain.Post;
+import com.Thispage.Thispage.Service.PostService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @PostMapping
+    public Post createPost(@RequestBody Post post) {
+        return postService.createPost(post);
+    }
+
+    @GetMapping("/{id}")
+    public Post findById(@PathVariable Long id) {
+        return postService.getPostById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Post updatePost(@PathVariable Long id, @RequestBody Post post) {
+        return postService.updatePost(id, post);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePost(@PathVariable Long id) {
+        postService.deletePost(id);
+    }
+}
---
+++ src/main/java/com/Thispage/Thispage/Service/PostService.java
@@ -0,0 +1,37 @@
+package com.Thispage.Thispage.Service;
+
+import com.Thispage.Thispage.Domain.Post;
+import com.Thispage.Thispage.Repository.PostRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+
+    private final PostRepository postRepository;
+
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    public Post createPost(Post post) {
+        return postRepository.save(post);
+    }
+
+    public Post getPostById(Long id) {
+        return postRepository.findById(id).orElse(null);
+    }
+
+    public Post updatePost(Long id, Post post) {
+        Post existingPost = postRepository.findById(id).orElse(null);
+        if (existingPost != null) {
+            existingPost.setTitle(post.getTitle());
+            existingPost.setContent(post.getContent());
+            return postRepository.save(existingPost);
+        }
+        return null;
+    }
+
+    public void deletePost(Long id) {
+        postRepository.deleteById(id);
+    }
+}
